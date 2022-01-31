2be7bf5956806b3085dcaccb404a7f450daad14e
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-31 11:16:14 -0600

    finished IntToString

--- src/IntToString.java
+++
@@ -1,7 +0,0 @@
-public class IntToString {
-
-    //You are given an integer , you have to convert it into a string.
-
-
-
-}
---
+++ src/finished/IntToString.java
@@ -0,0 +1,54 @@
+package finished;
+
+import java.util.*;
+import java.security.*;
+
+//given integer n, convert it to string
+//complete partially completed code
+//if code sucessfully converts n to a string s, code will print "good job" else will print "wrong answer"
+//n can range from -100 to 100 inclusive
+
+public class IntToString {
+    public static void main(String[] args) {
+
+        DoNotTerminate.forbidExit();
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in .nextInt();
+            in.close();
+
+            //Write your code here
+            String s = Integer.toString(n); //convert string to integer
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
